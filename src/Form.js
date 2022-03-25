95b15fcee2d6cf097ae01be05e4756c91743a988
Roger Wells <rsw359@gmail.com>
2022-03-25 11:05:18 +0900

    intanciated cards

--- src/Form.js
+++ src/Form.js
@@ -6,6 +6,7 @@ import Locard from './Locard'
 import './form.css'
 import ErrorModal from './Error'
 import Weather from './Weather'
+import Movie from './Movie'
 const API_KEY = process.env.REACT_APP_LOCATIONIQ_API_KEY;
 
 
@@ -17,6 +18,7 @@ class Explorer extends React.Component {
       data: {},
       location: {},
       weatherData: '',
+      movieData: '',
       errMessage: '',
       modalDataState: false,
     }
@@ -24,7 +26,7 @@ class Explorer extends React.Component {
 
 
   openModal = () => {
-    console.log('before', this.state)
+    // console.log('before', this.state)
     this.setState({
       modalDataState: true,
 
@@ -41,15 +43,15 @@ class Explorer extends React.Component {
     this.setState({
       location: e.target.value
     });
-    console.log(this.state.location);
+    // console.log(this.state.location);
   }
 
   getLoc = async (event) => {
     event.preventDefault()
     try {
       let cityData = await axios.get(`https://us1.locationiq.com/v1/search.php?key=${API_KEY}&q=${this.state.location}&format=json`);
-      console.log(cityData)
-      this.setState({ data: cityData.data[0]});
+      // console.log(cityData)
+      this.setState({ data: cityData.data[0] });
     } catch (e) {
       console.error(e);
       this.setState({
@@ -57,19 +59,27 @@ class Explorer extends React.Component {
       })
 
       this.openModal();
-      console.log(this.state.errMessage);///state is being set!
+      // console.log(this.state.errMessage);///state is being set!
       // this.openModal();
 
     }
-    console.log(this.state.data.lat)
-    console.log(this.state.data.lon)
-    let backEndCall = await axios.get(`http://localhost:3001/weather?lat=${this.state.data.lat}&lon=${this.state.data.lon}`);
-    console.log('line67',backEndCall.data[0].description);
-    this.setState({weatherData: backEndCall.data[0].description}); 
+    let weatherUrl = `${process.env.REACT_APP_SERVER}/weather?lat=${this.state.data.lat}&lon=${this.state.data.lon}`
+    let backEndCall = await axios.get(weatherUrl);  
+    this.setState({ weatherData: backEndCall.data});
+    
+    let url=`${process.env.REACT_APP_SERVER}/movies?city_name=${this.state.location}`;
+    console.log(url);
+    let movieBackendCall = await axios.get(url);
+    this.setState({
+    movieData: movieBackendCall.data
+    });
+    
   };
 
   render() {
+    console.log(this.state);
     console.log(this.state.weatherData);
+    console.log(this.state.movieData);
     return (
       <>
         <Form>
@@ -78,16 +88,16 @@ class Explorer extends React.Component {
           </Form.Label>
           <div className='search-container' >
 
-          <Form.Control className = 'input search-bar'
-            id='input'
-            onChange={this.handleChange}
-            type='text'
-            placeholder='Enter City' >
-          </Form.Control>
+            <Form.Control className='input search-bar'
+              id='input'
+              onChange={this.handleChange}
+              type='text'
+              placeholder='Enter City' >
+            </Form.Control>
 
-          <Button variant="primary" type="submit" onClick={this.getLoc}>
-            Explore!
-          </Button>
+            <Button variant="primary" type="submit" onClick={this.getLoc}>
+              Explore!
+            </Button>
           </div>
         </Form>
         {this.state.data.display_name ? (
@@ -100,13 +110,23 @@ class Explorer extends React.Component {
         <ErrorModal
           modalDataState={this.state.modalDataState}
           hideModal={this.hideModal}
-          errorCode={this.state.errMessage} pass err code
+          errorCode={this.state.errMessage} 
         />
-
-        <Weather
-        weatherData={this.state.weatherData}
-        />
-
+        {
+          this.state.weatherData && <Weather weatherData={this.state.weatherData}
+          />
+        }
+        
+        {
+          this.state.movieData && <Movie movies={this.state.movieData}/>          
+        }
+        
+        {/* {this.state.movieData ? (this.state.movieData[0].map((mov) => 
+          <Movie 
+          title={mov.title}
+          pic={mov.backdrop_path}
+          />
+         )) : null } */}
       </>
 
     )
---
+++ src/Movie.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { Card } from "react-bootstrap";
+
+
+class Movie extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {}
+  };
+
+  render() {
+    console.log(this.props.movies);
+    return(
+      <Card className='Card'  style={{ width: '50vh' }}>
+    
+    <Card.Body>       
+      <Card.Img src = {`https://image.tmdb.org/t/p/w500/${this.props.movies[0].pic}`} />
+      <Card.Text>
+      Movie Filmed Here:  {this.props.movies[0].title}     
+      </Card.Text>
+      <Card.Text>
+       {this.props.movies[1].title}      
+      </Card.Text>
+      <Card.Text>
+       {this.props.movies[2].title}      
+      </Card.Text>
+      
+    </Card.Body>
+  </Card>
+    )
+  }
+  
+};
+
+export default Movie
